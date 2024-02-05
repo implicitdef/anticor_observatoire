ec44126c6ce1a34845b55ad6119214319d4c54a8
implicitdef <implicitdef@users.noreply.github.com>
2024-02-05 12:06:50 +0100

    improve design

--- fc_website/src/components/ItemFiche.tsx
+++ fc_website/src/components/ItemFiche.tsx
@@ -5,15 +5,17 @@ import { TagsList } from './TagsList'
 
 export function ItemFiche({ item }: { item: Item }) {
   return (
-    <li className="flex flex-col bg-bleuanticor-500 text-white stretch justify-between px-4 pt-8 pb-8 ">
+    <li className="flex flex-col bg-gray-100 border-bleuanticor-100 border-l-4 border-0  stretch justify-between px-4 pt-8 pb-8 ">
       <div>
         {item.date ? (
-          <p className="mb-2">{formatDateVerbose(item.date)}</p>
+          <p className="mb-2 uppercase text-gray-500">
+            {formatDateVerbose(item.date)}
+          </p>
         ) : null}
-        <h2 className="font-bold text-2xl mb-2 ">
+        <h2 className="font-bold text-2xl mb-2 text-bleuanticor-500">
           <LinkToItem {...{ item }}>{item.titre}</LinkToItem>
         </h2>
-        <p className="mb-4">
+        <p className="mb-4 text-black">
           {shorten(item.contenu, 200)}{' '}
           <LinkToItem {...{ item }} className="ml-2 underline">
             Lire la suite&nbsp;→
