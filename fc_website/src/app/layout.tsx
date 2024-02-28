d37479a55beca3aa1fa234dbd37d7dc5c26c950f
implicitdef <implicitdef@users.noreply.github.com>
2024-02-28 21:34:51 +0100

    added social icons

--- fc_website/src/app/layout.tsx
+++ fc_website/src/app/layout.tsx
@@ -5,6 +5,10 @@ import 'remixicon/fonts/remixicon.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import logo from '@/img/logo.png'
+import logoMastodon from '@/img/sociallogos/mastodon.svg'
+import logoX from '@/img/sociallogos/x.svg'
+import logoInstagram from '@/img/sociallogos/instagram.svg'
+import logoFacebook from '@/img/sociallogos/facebook.png'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
@@ -24,31 +28,12 @@ export default function RootLayout({
         className={`${openSans.className} bg-white min-h-screen flex flex-col`}
       >
         <header className="">
-          <div className="container mx-auto border-b-2 border-0 border-solid border-gray-400">
-            <div className="flex justify-between pt-4">
-              <Link href="/" className="flex flex-col items-center">
-                <Image
-                  src={logo}
-                  width={130}
-                  height={61}
-                  alt="Logo de l'Observatoire, par Anticor"
-                />
-                <span className="font-bold text-2xl bg-bleuanticor-500 px-2 mt-2 text-white tracking-tight bg-bleXuanticor-100">
-                  l'observatoire
-                </span>
-              </Link>
-              <div className="bg-blue-300 min-w-[300px] m-4 flex items-center justify-center">
-                social links...
-              </div>
+          <div className="container mx-auto border-b-2 border-0 border-solid border-gray-400 flex justify-between">
+            <div className="">
+              <Logo />
+              <Menu />
             </div>
-            <div className="p-4 flex items-center gap-4 uppercase">
-              <Link href={'/revuedepresse'} className="">
-                Revue de presse
-              </Link>
-              <Link href={'/apropos'} className="">
-                À propos{' '}
-              </Link>
-            </div>
+            <SocialLinks />
           </div>
         </header>
         <main className="mx-auto grow w-full">{children}</main>
@@ -64,5 +49,68 @@ export default function RootLayout({
         </footer>
       </body>
     </html>
+  )
+}
+
+function SocialLinks() {
+  return (
+    <div className="flex items-start">
+      <div className="px-2 h-fit flex items-center gap-6 pt-4">
+        <a target="_blank" href="https://piaille.fr/@francecorruption">
+          <Image alt="Mastodon" src={logoMastodon} className="h-8 w-8" />
+        </a>
+        <a target="_blank" href="https://twitter.com/FR_corruption">
+          <Image alt="X" src={logoX} className="h-7 w-7" />
+        </a>
+        <a target="_blank" href="https://www.instagram.com/francecorruption/">
+          <Image alt="Instagram" src={logoInstagram} className="h-8 w-auto" />
+        </a>
+        <a target="_blank" href="https://www.facebook.com/FRcorruption/">
+          <Image
+            alt="Facebook"
+            src={logoFacebook}
+            className="h-8 w-auto"
+            width={32}
+            height={32}
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://francecorruption.fr/podcast/"
+          className="font-bold"
+        >
+          Podcast
+        </a>
+      </div>
+    </div>
+  )
+}
+
+function Logo() {
+  return (
+    <Link href="/" className="mt-2 ml-6 w-fit flex flex-col items-center">
+      <Image
+        src={logo}
+        width={130}
+        height={61}
+        alt="Logo de l'Observatoire, par Anticor"
+      />
+      <span className="font-bold text-2xl bg-bleuanticor-500 px-2 mt-2 text-white tracking-tight bg-bleXuanticor-100">
+        l'observatoire
+      </span>
+    </Link>
+  )
+}
+
+function Menu() {
+  return (
+    <div className="p-4 flex items-center gap-4 uppercase">
+      <Link href={'/revuedepresse'} className="">
+        Revue de presse
+      </Link>
+      <Link href={'/apropos'} className="">
+        À propos{' '}
+      </Link>
+    </div>
   )
 }
