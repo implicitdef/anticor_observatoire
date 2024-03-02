88ada87ecd6b808fbc729d08aefac6c94f769fb8
implicitdef <implicitdef@users.noreply.github.com>
2024-03-02 21:22:14 +0100

    rework setup of the link in articles list

--- fc_website/src/components/ItemFiche.tsx
+++ fc_website/src/components/ItemFiche.tsx
@@ -5,18 +5,20 @@ import { TagsList } from './TagsList'
 
 export function ItemFiche({ item }: { item: Item }) {
   return (
-    <li className="flex flex-col bg-gray-100 border-bleuanticor-100 border-l-4 border-0  stretch justify-between px-4 pt-8 pb-8 ">
-      <div>
-        {item.date ? (
-          <p className="mb-2 uppercase text-gray-500">
-            {formatDateVerbose(item.date)}
-          </p>
-        ) : null}
-        <h2 className="font-bold text-2xl mb-2 text-bleuanticor-500">
-          <LinkToItem {...{ item }}>{item.titre}</LinkToItem>
-        </h2>
-        <p className="mb-4 text-black">{item.contenu}</p>
-      </div>
+    <li className="flex flex-col bg-gray-100 border-bleuanticor-100 border-l-4 border-0  stretch justify-between px-4 pb-8 pt-4 ">
+      <LinkToItem {...{ item }} className="hover:bg-gray-200 ">
+        <div>
+          {item.date ? (
+            <p className="mb-2 uppercase text-gray-500">
+              {formatDateVerbose(item.date)}
+            </p>
+          ) : null}
+          <h2 className="font-bold text-2xl mb-2 text-bleuanticor-500">
+            {item.titre}
+          </h2>
+          <p className="mb-4 text-black">{item.contenu}</p>
+        </div>
+      </LinkToItem>
       <TagsList {...{ item }} />
     </li>
   )
