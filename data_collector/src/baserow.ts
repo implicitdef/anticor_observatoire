c82620b04ae780e6101302d55f944ff04d7d2c86
implicitdef <implicitdef@users.noreply.github.com>
2024-03-23 20:21:29 +0100

    migration vers baserow anticor

--- data_collector/src/baserow.ts
+++ data_collector/src/baserow.ts
@@ -2,9 +2,9 @@ import { getBaserowApiToken } from './utils'
 import axios from 'axios'
 import { z } from 'zod'
 
-const MAIN_TABLE_ID = '223611' // nommée "Import site 26/11/23"
+const MAIN_TABLE_ID = '517' // nommée "Import site 26/11/23"
 const ONE_PAGE_ONLY = false // to go quicker in dev
-const ORDER_BY = `-field_1556851,field_1556848` // order by date desc, then by title
+const ORDER_BY = `-field_4799,field_4796` // order by date desc, then by title
 
 export async function readBaserow(): Promise<NiceRow[]> {
   const res = (await readTable()) as Row[]
@@ -13,7 +13,7 @@ export async function readBaserow(): Promise<NiceRow[]> {
 }
 
 async function readTable() {
-  const firstPageUrl = `https://api.baserow.io/api/database/rows/table/${MAIN_TABLE_ID}/?size=200&order_by=${ORDER_BY}`
+  const firstPageUrl = `https://baserow.anticor.org/api/database/rows/table/${MAIN_TABLE_ID}/?size=200&order_by=${ORDER_BY}`
 
   async function inner(nextPageUrl?: string): Promise<Row[]> {
     const url = nextPageUrl ?? firstPageUrl
@@ -78,19 +78,19 @@ const procedureSchema = z
 const rowSchema = z.object({
   id: z.number(),
   order: z.string(),
-  field_1556848: z.string(),
-  field_1556849: z.string(),
-  field_1556850: z.string(),
-  field_1556851: z.string().nullable(),
-  field_1556852: z.string(),
-  field_1556853: departementSchema.array(),
-  field_1557648: personnaliteSchema.array(),
-  field_1557650: personneMoraleSchema.array(),
-  field_1557681: categorieSchema.array(),
-  field_1557682: themeSchema.array(),
-  field_1561023: procedureSchema.array(),
-  field_1751584: z.string().nullable(),
-  field_1772289: z.boolean(),
+  field_4796: z.string(),
+  field_4801: z.string(),
+  field_4800: z.string(),
+  field_4799: z.string().nullable(),
+  field_4816: z.string(),
+  field_4805: departementSchema.array(),
+  field_4809: personnaliteSchema.array(),
+  field_4814: personneMoraleSchema.array(),
+  field_4802: categorieSchema.array(),
+  field_4803: themeSchema.array(),
+  field_4804: procedureSchema.array(),
+  field_4797: z.string().nullable(),
+  field_4798: z.boolean(),
 })
 // Not strict, because a new column may be added
 //.strict()
@@ -109,36 +109,36 @@ export type NiceRow = ReturnType<typeof mapRow>
 function mapRow(row: Row) {
   const {
     id,
-    field_1556848,
-    field_1556849,
-    field_1556850,
-    field_1556851,
-    field_1556852,
-    field_1556853,
-    field_1557648,
-    field_1557650,
-    field_1557681,
-    field_1557682,
-    field_1561023,
-    field_1751584,
-    field_1772289,
+    field_4796,
+    field_4801,
+    field_4800,
+    field_4799,
+    field_4816,
+    field_4805,
+    field_4809,
+    field_4814,
+    field_4802,
+    field_4803,
+    field_4804,
+    field_4797,
+    field_4798,
   } = row
 
   return {
     id,
-    titre: field_1556848,
-    contenu: field_1556849,
-    url: field_1556850,
-    date: field_1556851,
-    etiquettes: field_1556852,
-    departement: removeColors(field_1556853),
-    personnalites: field_1557648,
-    personnes_morales: field_1557650,
-    categorie: removeColors(field_1557681),
-    theme: removeColors(field_1557682),
-    procedure: removeColors(field_1561023),
-    titre_corrige: field_1751584,
-    a_la_une: field_1772289,
+    titre: field_4796,
+    contenu: field_4801,
+    url: field_4800,
+    date: field_4799,
+    etiquettes: field_4816,
+    departement: removeColors(field_4805),
+    personnalites: field_4809,
+    personnes_morales: field_4814,
+    categorie: removeColors(field_4802),
+    theme: removeColors(field_4803),
+    procedure: removeColors(field_4804),
+    titre_corrige: field_4797,
+    a_la_une: field_4798,
   }
 }
 
